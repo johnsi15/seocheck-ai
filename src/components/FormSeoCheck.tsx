feab2304a88911e6fde99f2b9dd247a906cb402d
John Serrano <andrey_201015@hotmail.com>
2023-12-20 18:01:08 -0500

    Add react hook form

--- src/components/FormSeoCheck.tsx
+++ src/components/FormSeoCheck.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { ChangeEventHandler, FormEventHandler, useState } from 'react'
-import { set, z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 const SeoSchema = z.object({
   title: z
@@ -27,69 +28,25 @@ const SeoSchema = z.object({
 type SeoCheck = z.infer<typeof SeoSchema>
 
 export function FormSeoCheck() {
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [keyword, setKeyword] = useState('')
-  const [errores, setErrores] = useState({
-    title: {
-      message: '',
-      error: false,
-    },
-    description: {
-      error: false,
-    },
-  })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues,
+  } = useForm<SeoCheck>()
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = event => {
-    const { name, value } = event.target
-    // console.log({ name, value })
-    if (name === 'title') {
-      const len = value.length
-
-      if (len === 0) {
-        setTitle(value)
-        setErrores({
-          ...errores,
-          title: {
-            message: '',
-            error: false,
-          },
-        })
-        return
-      }
-
-      setTitle(value)
-      const res = SeoSchema.shape.title.safeParse(value)
-      if (!res.success && len > 30) {
-        const { message } = res.error.issues[0]
-        setErrores({
-          ...errores,
-          title: {
-            message,
-            error: true,
-          },
-        })
-
-        return
-      }
-
-      setErrores({
-        ...errores,
-        title: {
-          message: '',
-          error: false,
-        },
-      })
-    }
-    if (name === 'description') setDescription(value)
-    if (name === 'keyword') setKeyword(value)
-    console.log({ title, description, keyword })
-  }
+  // const handleChange: ChangeEventHandler<HTMLInputElement> = event => {
 
   // const handleSubmit: FormEventHandler<HTMLFormElement> = event => {}
 
+  const onSubmit = (data: SeoCheck) => {
+    console.log('onsubmit')
+    reset()
+  }
+
   return (
-    <form className='flex flex-col gap-5 w-[700px]'>
+    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5 w-[700px]'>
       <label htmlFor='title' className='flex flex-col'>
         <span className='mb-2'>
           Ingresa un título descriptivo para tu contenido. Asegúrate de que sea claro y relevante para mejorar la
@@ -98,18 +55,26 @@ export function FormSeoCheck() {
         <input
           id='title'
           type='text'
-          name='title'
           placeholder='Título'
           className={`w-full pl-5 pr-3 py-2   outline-none border  shadow-sm rounded-lg ${
-            errores.title.error
+            errors.title
               ? 'bg-red-50 border-red-500 text-red-900 focus:border-red:500'
               : 'bg-transparent text-gray-500 focus:border-indigo-600'
           }`}
-          onChange={handleChange}
-          value={title}
-          required
+          {...register('title', {
+            required: 'El título es requerido',
+            minLength: {
+              value: 55,
+              message:
+                'El título debe tener al menos 55 caracteres para mejorar la visibilidad en motores de búsqueda.',
+            },
+            maxLength: {
+              value: 75,
+              message: 'El título no se recomienda que tenga más de 75 caracteres.',
+            },
+          })}
         />
-        {errores.title.error && <p className='text-red-900 text-base mt-3'>{errores.title.message}</p>}
+        {errors.title && <p className='text-red-900 text-base mt-3'>{errors.title.message}</p>}
       </label>
       <label htmlFor='description' className='flex flex-col'>
         <span className='mb-2'>
@@ -119,13 +84,15 @@ export function FormSeoCheck() {
         <input
           id='description'
           type='text'
-          name='description'
           placeholder='Descripción'
-          className='w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg'
-          onChange={handleChange}
-          value={description}
-          required
+          className={`w-full pl-5 pr-3 py-2   outline-none border  shadow-sm rounded-lg ${
+            errors.description
+              ? 'bg-red-50 border-red-500 text-red-900 focus:border-red:500'
+              : 'bg-transparent text-gray-500 focus:border-indigo-600'
+          }`}
+          {...register('description', { required: 'La descripción es requerida' })}
         />
+        {errors.description && <p className='text-red-900 text-base mt-3'>{errors.description.message}</p>}
       </label>
       <label htmlFor='keyword' className='flex flex-col'>
         <span className='mb-2'>
@@ -135,14 +102,13 @@ export function FormSeoCheck() {
         <input
           id='keyword'
           type='text'
-          name='keyword'
           placeholder='Palabra clave'
           className='w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg'
-          onChange={handleChange}
-          value={keyword}
+          {...register('keyword')}
         />
       </label>
       <button
+        disabled={isSubmitting}
         type='submit'
         className='px-6 py-3.5 text-white bg-indigo-600 rounded-lg duration-150 hover:bg-indigo-700 active:shadow-lg'
       >
