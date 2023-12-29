3d7482d64ca2278769fe184ba442e7137c3574a8
John Serrano <andrey_201015@hotmail.com>
2023-12-29 15:23:48 -0500

    Add icon error and success

--- src/components/FormSeoCheck.tsx
+++ src/components/FormSeoCheck.tsx
@@ -2,12 +2,14 @@
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { SeoCheck, seoCheckSchema } from '@/lib/types'
+import { IconSuccess } from './IconSuccess'
+import { IconError } from './IconError'
 
 export function FormSeoCheck() {
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting, isValid, isValidating },
+    formState: { errors, isSubmitting, isValid },
     reset,
     getFieldState,
   } = useForm<SeoCheck>({
@@ -20,9 +22,6 @@ export function FormSeoCheck() {
     // reset()
   }
 
-  const fieldState = getFieldState('title')
-  console.log({ fieldState })
-
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5 w-[700px]'>
       <label htmlFor='title' className='flex flex-col'>
@@ -30,17 +29,29 @@ export function FormSeoCheck() {
           Ingresa un título descriptivo para tu contenido. Asegúrate de que sea claro y relevante para mejorar la
           visibilidad en los motores de búsqueda.
         </span>
-        <input
-          id='title'
-          type='text'
-          placeholder='Título'
-          className={`w-full pl-5 pr-3 py-2   outline-none border  shadow-sm rounded-lg ${
-            errors.title
-              ? 'bg-red-50 border-red-500 text-red-900 focus:border-red:500'
-              : 'bg-transparent text-gray-500 focus:border-indigo-60'
-          } ${isValidating && !errors.title ? 'border-green-500 text-green-900' : ''}`}
-          {...register('title')}
-        />
+        <div className='relative'>
+          <input
+            id='title'
+            type='text'
+            placeholder='Título'
+            className={`w-full pl-5 pr-3 py-2   outline-none border  shadow-sm rounded-lg ${
+              errors.title
+                ? 'bg-red-50 border-red-500 text-red-900 focus:border-red:500'
+                : 'bg-transparent text-gray-500 focus:border-indigo-600'
+            } ${!getFieldState('title').invalid && getFieldState('title').isDirty ? 'border-green-500' : ''}`}
+            {...register('title')}
+          />
+          {getFieldState('title').isDirty && !getFieldState('title').invalid && (
+            <div className='absolute inset-y-0 -end-10 flex items-center ps-3 pointer-events-none'>
+              <IconSuccess />
+            </div>
+          )}
+          {errors.title && (
+            <div className='absolute inset-y-0 -end-10 flex items-center ps-3 pointer-events-none'>
+              <IconError />
+            </div>
+          )}
+        </div>
         {errors.title && <p className='text-red-900 text-base mt-3'>{errors.title.message}</p>}
       </label>
       <label htmlFor='description' className='flex flex-col'>
@@ -48,17 +59,32 @@ export function FormSeoCheck() {
           Proporciona una descripción detallada de tu contenido. Esto ayudará a los motores de búsqueda y a los usuarios
           a entender de qué se trata tu página.
         </span>
-        <textarea
-          id='description'
-          rows={3}
-          placeholder='Descripción'
-          className={`w-full pl-5 pr-3 py-2   outline-none border  shadow-sm rounded-lg resize-none ${
-            errors.description
-              ? 'bg-red-50 border-red-500 text-red-900 focus:border-red:500'
-              : 'bg-transparent text-gray-500 focus:border-indigo-600'
-          }`}
-          {...register('description')}
-        ></textarea>
+        <div className='relative'>
+          <textarea
+            id='description'
+            rows={3}
+            placeholder='Descripción'
+            className={`w-full pl-5 pr-3 py-2   outline-none border  shadow-sm rounded-lg resize-none ${
+              errors.description
+                ? 'bg-red-50 border-red-500 text-red-900 focus:border-red:500'
+                : 'bg-transparent text-gray-500 focus:border-indigo-600'
+            } ${
+              !getFieldState('description').invalid && getFieldState('description').isDirty ? 'border-green-500' : ''
+            }`}
+            {...register('description')}
+          ></textarea>
+
+          {getFieldState('description').isDirty && !getFieldState('description').invalid && (
+            <div className='absolute inset-y-0 -end-10 flex items-center ps-3 pointer-events-none'>
+              <IconSuccess />
+            </div>
+          )}
+          {errors.description && (
+            <div className='absolute inset-y-0 -end-10 flex items-center ps-3 pointer-events-none'>
+              <IconError />
+            </div>
+          )}
+        </div>
         {errors.description && <p className='text-red-900 text-base mt-3'>{errors.description.message}</p>}
       </label>
       <label htmlFor='keyword' className='flex flex-col'>
---
+++ src/components/IconError.tsx
@@ -0,0 +1,34 @@
+export function IconError() {
+  return (
+    <svg
+      className='w-8 h-8 text-gray-500 dark:text-gray-400'
+      xmlns='http://www.w3.org/2000/svg'
+      x='0'
+      y='0'
+      viewBox='13.9 13.9 36.3 36.3'
+    >
+      <circle
+        cx='32'
+        cy='32'
+        r='17.5'
+        fill='#FFF'
+        stroke='#7F0013FF'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeMiterlimit='10'
+        strokeWidth='1.3'
+      ></circle>
+      <g
+        fill='none'
+        stroke='#7F0013FF'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeMiterlimit='10'
+        strokeWidth='1.3'
+      >
+        <path d='M39 25L25 39'></path>
+        <path d='M39 39L25 25'></path>
+      </g>
+    </svg>
+  )
+}
