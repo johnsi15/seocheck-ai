0dbd2d4e029ce09c46e6426e84ddc3a17f64e3b3
John Serrano <andrey_201015@hotmail.com>
2024-01-15 17:45:46 -0500

    Add buy coffee button header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,6 +11,18 @@ export function Header() {
           </a>
         </div>
         <ul className={`flex-1  mt-12 md:flex md:mt-0 items-center justify-end gap-1`}>
+          <li className='pb-5 md:pb-0 bg-slate-400 dark:bg-slate-200 rounded hover:opacity-90'>
+            <a
+              href='https://www.buymeacoffee.com/jandrey15'
+              className='flex gap-1 text-gray-900 dark:text-gray-800 px-2 py-1'
+              aria-label='Cómprame un café'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img src='/bmc-logo.svg' alt='Cómprame un café' width={20} height={20} />
+              Cómprame un café
+            </a>
+          </li>
           <li className='pb-5 md:pb-0 hover:opacity-75 w-10 h-10 flex justify-center items-center focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg'>
             <a
               className='p-2'
