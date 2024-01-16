9e42428be766f14bc44dcd610afe780a6cd93652
John Serrano <andrey_201015@hotmail.com>
2024-01-16 17:51:11 -0500

    Add meta data openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,36 @@ const lato = Lato({ weight: ['300', '400', '700', '900'], subsets: ['latin'], di
 export const metadata: Metadata = {
   title: 'SEO CheckAI Potencia la visibilidad de tu contenido',
   description: 'Descubre el poder de SEO CheckAI para impulsar la visibilidad de tu contenido en línea',
+  generator: 'Next.js',
+  applicationName: 'SEOCheckAI',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['seo', 'buenas-practicas', 'AI', 'IA', 'Inteligencia Artificial'],
+  authors: [{ name: 'John Serrano', url: 'https://johnserrano.co/' }],
+  creator: 'John Serrano',
+  publisher: 'John Serrano',
+  // formatDetection: {
+  //   email: false,
+  //   address: false,
+  //   telephone: false,
+  // },
+  openGraph: {
+    title: 'SEO CheckAI Potencia la visibilidad de tu contenido',
+    description: 'Descubre el poder de SEO CheckAI para impulsar la visibilidad de tu contenido en línea',
+    images: ['/share.jpg'],
+    locale: 'es_LA',
+    type: 'website',
+    url: 'https://johnserrano.co/', // url site now
+    siteName: 'SEOCheckAI',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'SEO CheckAI Potencia la visibilidad de tu contenido',
+    description: 'Descubre el poder de SEO CheckAI para impulsar la visibilidad de tu contenido en línea',
+    // siteId: '1467726470533754880',
+    creator: '@Jandrey15',
+    creatorId: '1467726470533754880',
+    images: ['https://nextjs.org/og.png'], // Must be an absolute URL
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
