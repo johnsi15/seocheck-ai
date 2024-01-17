016969d6ddb1a437089d5507f2fda6c0958d69cc
John Serrano <andrey_201015@hotmail.com>
2024-01-17 16:21:27 -0500

    add script GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Lato } from 'next/font/google'
 import './globals.css'
 import { ThemeProviders } from '@/components/ThemeProviders'
@@ -38,6 +39,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
         </ThemeProviders>
       </body>
+      <Script src='https://www.googletagmanager.com/gtag/js?id=G-F4NC0KEW3E' />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-F4NC0KEW3E');
+        `}
+      </Script>
     </html>
   )
 }
