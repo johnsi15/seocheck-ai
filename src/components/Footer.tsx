01dcc8a7908d2f932e39a205d8d1f9b7f0769246
John Serrano <andrey_201015@hotmail.com>
2024-01-21 15:51:25 -0500

    Changes styles footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,42 +1,54 @@
+import { IconHeart } from './Icons'
 export function Footer() {
   return (
-    <footer className='bg-white rounded-lg shadow dark:bg-transparent m-4'>
+    <footer className='bg-slate-900 shadow dark:bg-transparent mt-[100px]'>
       <div className='w-full max-w-screen-xl mx-auto p-4 md:py-8'>
         <div className='sm:flex sm:items-center sm:justify-between'>
-          <a href='https://flowbite.com/' className='flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse'>
-            <img src='https://flowbite.com/docs/images/logo.svg' className='h-8' alt='Flowbite Logo' />
-            <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>SEOcheckAI</span>
+          <a href='/' className='flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse'>
+            {/* <img src='https://flowbite.com/docs/images/logo.svg' className='h-8' alt='Flowbite Logo' /> */}
+            <span className='self-center text-2xl font-semibold whitespace-nowrap text-slate-200 dark:text-white'>
+              SEOcheckAI
+            </span>
           </a>
-          <ul className='flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400'>
+          <ul className='flex flex-wrap items-center mb-6 text-sm font-medium text-slate-200 sm:mb-0 dark:text-gray-300'>
             <li>
-              <a href='#' className='hover:underline me-4 md:me-6'>
+              <a
+                href='https://link.mercadopago.com.co/johnserrano'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='hover:underline me-4 md:me-6'
+              >
                 Donar
               </a>
             </li>
             <li>
-              <a href='#' className='hover:underline me-4 md:me-6'>
+              <a href='/validar-seo' className='hover:underline me-4 md:me-6'>
                 Validar SEO
               </a>
             </li>
             <li>
-              <a href='#' className='hover:underline me-4 md:me-6'>
+              <a
+                href='https://www.buymeacoffee.com/jandrey15'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='hover:underline me-4 md:me-6'
+              >
                 Cómprame un cáfe
               </a>
             </li>
             <li>
-              <a href='#' className='hover:underline'>
+              <a href='mailto:me@johnserrano.co' target='_blank' rel='noopener noreferrer' className='hover:underline'>
                 Contacto
               </a>
             </li>
           </ul>
         </div>
         <hr className='my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8' />
-        <span className='block text-sm text-gray-500 sm:text-center dark:text-gray-400'>
-          Desarrollado con 💖 por
-          <a href='https://johnserrano.co/' className='hover:underline'>
+        <span className='text-sm text-slate-200 sm:text-center dark:text-gray-300 flex gap-1 justify-center'>
+          Desarrollado con <IconHeart /> por{' '}
+          <a href='https://johnserrano.co/' className='hover:underline font-semibold'>
             John Serrano
-          </a>{' '}
-          © 2023
+          </a>
         </span>
       </div>
     </footer>
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -110,3 +110,26 @@ export function IconGitHub({ height = 20, width = 20 }: { height?: number; width
     </svg>
   )
 }
+
+export function IconHeart({ height = 20, width = 20 }: { height?: number; width?: number }) {
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className='block'
+      height={height}
+      width={width}
+    >
+      <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+      <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
+      <g id='SVGRepo_iconCarrier'>
+        {' '}
+        <path
+          d='M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z'
+          fill='#c90d0d'
+        ></path>{' '}
+      </g>
+    </svg>
+  )
+}
