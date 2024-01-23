fe36ea5d1e04ee0018e285ece093d0d338cdd0af
John Serrano <andrey_201015@hotmail.com>
2024-01-23 08:24:24 -0500

    Changes styles Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,13 +9,13 @@ export function Banner() {
     <div
       id='sticky-banner'
       tabIndex={-1}
-      className='fixed bottom-0 start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600'
+      className='fixed bottom-0 start-0 z-50 flex justify-between w-full p-4 border-t border-gray-400 bg-slate-300 dark:bg-blue-950 dark:border-gray-800'
     >
       <div className='flex items-center mx-auto'>
-        <p className='flex items-center text-sm font-normal text-gray-500 dark:text-slate-200'>
-          <span className='inline-flex p-1 me-3 bg-gray-200 rounded-full dark:bg-gray-600 w-6 h-6 items-center justify-center flex-shrink-0'>
+        <p className='flex items-center text-sm font-normal text-gray-800 dark:text-slate-200'>
+          <span className='inline-flex p-1 me-3 bg-gray-900 rounded-full dark:bg-slate-900 w-6 h-6 items-center justify-center flex-shrink-0'>
             <svg
-              className='w-3 h-3 text-gray-500 dark:text-slate-200'
+              className='w-3 h-3 text-slate-200 dark:text-slate-200'
               aria-hidden='true'
               xmlns='http://www.w3.org/2000/svg'
               fill='currentColor'
@@ -41,7 +41,7 @@ export function Banner() {
         <button
           data-dismiss-target='#sticky-banner'
           type='button'
-          className='flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white'
+          className='flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-800 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white'
           onClick={handleClose}
         >
           <svg
