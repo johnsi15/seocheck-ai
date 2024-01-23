7a32fd5935d6d04cb0ed500d163f8cae35524c5c
John Serrano <andrey_201015@hotmail.com>
2024-01-23 08:26:28 -0500

    Changes style hover theme switcher

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -24,7 +24,7 @@ export function ThemeSwitcher() {
       id='theme-toggle'
       type='button'
       onClick={handleToggleTheme}
-      className='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5'
+      className='text-gray-500 dark:text-gray-400 hover:bg-gray-300 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5'
     >
       {resolvedTheme === 'dark' ? (
         <svg
