aad3f755e3630c92491474d984ac7fc42c3727c1
John Serrano <andrey_201015@hotmail.com>
2024-01-23 15:10:06 -0500

    Add styles mobile Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { ThemeSwitcher } from './ThemeSwitcher'
 export function Header() {
   return (
     <header className='h-max border-b-2 border-gray-300 dark:border-gray-800 p-1 px-0 '>
-      <nav className='items-center justify-between px-4 mx-auto max-w-screen-xl sm:px-8 md:flex md:space-x-6 h-14'>
+      <nav className='items-center justify-between px-1 md:px-4 mx-auto max-w-screen-xl sm:px-8 flex gap-1 md:gap-0 md:space-x-6 h-14'>
         <div className='flex justify-between'>
           <a className='no-underline' href='/' aria-label='Home seocheckai'>
             <img
@@ -23,11 +23,11 @@ export function Header() {
             />
           </a>
         </div>
-        <ul className={`flex-1  mt-12 md:flex md:mt-0 items-center justify-end gap-1`}>
-          <li className='pb-5 md:pb-0 bg-slate-400 dark:bg-slate-200 rounded hover:opacity-90'>
+        <ul className={`md:flex-1 flex md:mt-0 items-center justify-end gap-1`}>
+          <li className='md:pb-0 bg-slate-400 dark:bg-slate-200 rounded hover:opacity-90'>
             <a
               href='https://www.buymeacoffee.com/jandrey15'
-              className='flex gap-1 text-gray-900 dark:text-gray-800 px-2 py-1 items-center justify-center'
+              className='flex gap-1 text-gray-900 dark:text-gray-800 px-2 py-1 items-center justify-center text-xs md:text-base'
               aria-label='Cómprame un café'
               target='_blank'
               rel='noopener noreferrer'
@@ -36,7 +36,7 @@ export function Header() {
               Cómprame un café
             </a>
           </li>
-          <li className='pb-5 md:pb-0 hover:opacity-75 w-10 h-10 flex justify-center items-center focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg'>
+          <li className='md:pb-0 hover:opacity-75 w-10 h-10 flex justify-center items-center focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg'>
             <a
               className='p-2'
               href='https://github.com/johnsi15'
@@ -47,7 +47,7 @@ export function Header() {
               <IconGitHub />
             </a>
           </li>
-          <li className='pb-5 md:pb-0 w-10 h-10'>
+          <li className='md:pb-0 w-10 h-10'>
             <ThemeSwitcher />
           </li>
         </ul>
