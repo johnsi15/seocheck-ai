02bd1e4ed430ebd9588669409f81774bc9ffba58
John Serrano <andrey_201015@hotmail.com>
2024-07-17 18:31:14 -0500

    Change endpoint suggestions for 3

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,8 +10,13 @@ if (!process.env.OPENAI_API_KEY) {
 const defaultMessages: CoreMessage[] = [
   {
     role: 'system',
-    content:
-      'You are a helpful assistant that provides SEO suggestions. The goal is to optimize the title and description for search engines. Titles should be between 55 and 80 characters, and descriptions should be between 120 and 160 characters. If a keyword or keywords are provided, ensure that it appears in both the title and description. The answer must be in Spanish.',
+    content: `Como un asistente útil que proporciona sugerencias de SEO, tu objetivo es optimizar el título y la descripción para los motores de búsqueda. Debes proporcionar tres opciones de títulos y tres opciones de descripciones para cada entrada.
+
+      Los títulos deben tener entre 55 y 80 caracteres.
+      Las descripciones deben tener entre 120 y 160 caracteres.
+      Si se proporcionan una palabra clave o palabras clave, asegúrate de que aparezcan en ambas, el título y la descripción.
+      Proporciona las sugerencias en español.
+    `,
   },
   {
     role: 'user',
@@ -19,23 +24,7 @@ const defaultMessages: CoreMessage[] = [
   },
   {
     role: 'assistant',
-    content: `Título: De la ilusión a la angustia: El ‘sueño americano’ de una familia cucuteña\nDescripción: Familia cucuteña vive angustia en travesía a EE. UU. Padres ruegan ayuda para rescatar seres queridos secuestrados en odisea del ‘sueño americano’.`,
-  },
-  {
-    role: 'user',
-    content: `Title: De la ilusión a la angustia: El ‘sueño americano’ convertido en pesadilla para una familia cucuteña\nDescription: Padres cucuteños ruegan ayuda para rescatar a su familia secuestrada en su ruta a los Estados Unidos.`,
-  },
-  {
-    role: 'assistant',
-    content: `Título: Pesadilla en ruta: Familia cucuteña ruega ayuda para rescatar a seres queridos\nDescripción: Esperanza de familia cucuteña se torna desesperación. Padres ruegan asistencia para liberar seres queridos secuestrados en peligrosa ruta hacia Estados Unidos.`,
-  },
-  {
-    role: 'user',
-    content: `Title: De la ilusión a la angustia: El ‘sueño americano’ convertido en pesadilla para una familia cucuteña\nDescription: Padres cucuteños ruegan ayuda para rescatar a su familia secuestrada en su ruta a los Estados Unidos.\n Keywords: familia cucuteña`,
-  },
-  {
-    role: 'assistant',
-    content: `Título: De la ilusión a la angustia: El ‘Sueño Americano’ de una familia cucuteña\nDescripción: Familia cucuteña vive angustia en travesía a EE. UU. Padres ruegan ayuda para rescatar seres queridos secuestrados en odisea del ‘Sueño Americano’.\nPalabra Clave: familia cucuteña`,
+    content: `Título:: De la ilusión a la angustia: El ‘sueño americano’ de una familia cucuteña\nDescripción:: Familia cucuteña vive angustia en travesía a EE. UU. Padres ruegan ayuda para rescatar seres queridos secuestrados en odisea del ‘sueño americano’.\nkeywords:: ilusión, angustia, sueño americano, familia cucuteña<break />Título:: El ‘sueño americano’ roto: La travesía angustiante de una familia cucuteña\nDescripción:: Familia cucuteña enfrenta angustia en su travesía hacia EE. UU. Padres desesperados buscan ayuda para rescatar a sus seres queridos secuestrados en la odisea del ‘sueño americano’.\nkeywords:: sueño americano roto, travesía angustiante, familia cucuteña<break />Título:: De Cúcuta a la desilusión la lucha de una familia por el ‘sueño americano’\nDescripción:: En su travesía hacia EE. UU., una familia cucuteña vive una angustiante odisea. Padres ruegan por ayuda para rescatar a sus seres queridos secuestrados en la búsqueda del ‘sueño americano’.keywords:: Cúcuta, desilusión, lucha, sueño americano, familia`,
   },
 ]
 
