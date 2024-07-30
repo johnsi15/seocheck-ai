07bee8ff221adf7b84267db3da6a8bcea9ba2203
John Serrano <andrey_201015@hotmail.com>
2024-07-30 18:16:49 -0500

    Add invalida web audit component

---
+++ src/components/InvalidWebAudit.tsx
@@ -0,0 +1,17 @@
+import { Header } from '@/components/Header'
+import { WebAuditForm } from '@/components/WebAuditForm'
+
+export function InvalidWebAudit() {
+  return (
+    <>
+      <Header />
+      <main className='flex flex-col items-center pt-16 pb-10 px-4 md:px-0 w-full md:m-auto md:w-[700px]'>
+        <h1 className='text-4xl dark:text-slate-200 mb-10 text-center'>
+          Audita el <span className='dark:text-rose-600 text-rose-700'>SEO</span> de tu sitio web 🔥
+        </h1>
+        <p className='mb-4 text-lg'>Es necesario una URL</p>
+        <WebAuditForm />
+      </main>
+    </>
+  )
+}
