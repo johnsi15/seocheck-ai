f4fda77cad32bb4b3236fcde00fd790fdf56ba62
John Serrano <andrey_201015@hotmail.com>
2024-07-30 18:17:14 -0500

    Add score and new field scraping

--- src/app/auditar-seo/page.tsx
+++ src/app/auditar-seo/page.tsx
@@ -1,5 +1,5 @@
 import { Header } from '@/components/Header'
-import { WebAuditForm } from '@/components/WebAuditForm'
+import { InvalidWebAudit } from '@/components/InvalidWebAudit'
 import { webScraping } from '@/lib/scrapingPly'
 
 export default async function AuditSeo({
@@ -10,22 +10,20 @@ export default async function AuditSeo({
   const { url } = searchParams
 
   if (!url || typeof url !== 'string' || !url.startsWith('https://')) {
-    return (
-      <>
-        <Header />
-        <main className='flex flex-col items-center pt-16 pb-10 px-4 md:px-0 w-full md:m-auto md:w-[700px]'>
-          <h1 className='text-4xl dark:text-slate-200 mb-10 text-center'>
-            Audita el <span className='dark:text-rose-600 text-rose-700'>SEO</span> de tu sitio web 🔥
-          </h1>
-          <p className='mb-4 text-lg'>Es necesario una URL</p>
-          <WebAuditForm />
-        </main>
-      </>
-    )
+    return <InvalidWebAudit />
   }
 
-  const result = await webScraping({ url })
-  console.log({ result })
+  const { score } = await webScraping({ url })
+
+  const listRevision = [
+    'Título',
+    'Descripción',
+    'H1',
+    'H2',
+    'Alt en la imágenes',
+    'Enlaces internos',
+    'Marcado de esquema',
+  ]
 
   return (
     <>
@@ -34,6 +32,49 @@ export default async function AuditSeo({
         <h1 className='text-4xl dark:text-slate-200 mb-10 text-center'>
           Audita el <span className='dark:text-rose-600 text-rose-700'>SEO</span> de tu sitio web 🔥
         </h1>
+
+        <div className='flex items-center flex-col'>
+          <div className='relative size-40'>
+            <svg className='rotate-[135deg] size-full' viewBox='0 0 36 36' xmlns='http://www.w3.org/2000/svg'>
+              <circle
+                cx='18'
+                cy='18'
+                r='16'
+                fill='none'
+                className='stroke-current text-green-200 dark:text-neutral-700'
+                strokeWidth='1'
+                strokeDasharray='75 100'
+                strokeLinecap='round'
+              ></circle>
+
+              <circle
+                cx='18'
+                cy='18'
+                r='16'
+                fill='none'
+                className='stroke-current text-green-500 dark:text-green-500'
+                strokeWidth='2'
+                strokeDasharray={score}
+                strokeLinecap='round'
+              ></circle>
+            </svg>
+
+            <div className='absolute top-1/2 start-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
+              <span className='text-4xl font-bold text-green-600 dark:text-green-500'>{score}</span>
+              <span className='text-green-600 dark:text-green-500 block'>Puntaje</span>
+            </div>
+          </div>
+          <p className='mt-2'>Puntuación de SEO en la página</p>
+        </div>
+
+        <div>
+          Se reviso:
+          <ul>
+            {listRevision.map(item => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
       </main>
     </>
   )
--- src/lib/scrapingPly.ts
+++ src/lib/scrapingPly.ts
@@ -10,15 +10,31 @@ export async function webScraping({ url }: { url: string }) {
 
   const title = await page.title()
 
-  // Ejemplo de cómo extraer la meta description
+  const titles = await page.$$eval('title', titles => titles.map(title => title.textContent))
+
   const description = await page
-    .$eval('meta[name="description"]', element => element.content)
+    .$eval('meta[name="description"]', (element: HTMLMetaElement) => element.content)
     .catch(() => 'No description found')
 
-  // Ejemplo de cómo extraer H1
-  const h1 = await page.$eval('h1', element => element.innerText).catch(() => 'No H1 found')
+  const OGtitle = await page
+    .$eval('meta[name="og:title"]', (element: HTMLMetaElement) => element.content)
+    .catch(() => 'No og:title found')
 
-  // Ejemplo de cómo extraer alt text de imágenes
+  const OGdescription = await page
+    .$eval('meta[name="og:description"]', (element: HTMLMetaElement) => element.content)
+    .catch(() => 'No og:description found')
+
+  const OGimage = await page
+    .$eval('meta[name="og:image"]', (element: HTMLMetaElement) => element.content)
+    .catch(() => 'No og:image found')
+
+  const OGurl = await page
+    .$eval('meta[name="og:url"]', (element: HTMLMetaElement) => element.content)
+    .catch(() => 'No og:url found')
+
+  const h1 = await page.$eval('h1', element => element.innerText).catch(() => 'No H1 found')
+  const h2 = await page.$eval('h2', element => element.innerText).catch(() => 'No H2 found')
+
   const images = await page.$$eval('img', imgs =>
     imgs.map(img => ({
       src: img.src,
@@ -26,7 +42,6 @@ export async function webScraping({ url }: { url: string }) {
     }))
   )
 
-  // Ejemplo de cómo extraer los links internos y externos
   const links = await page.$$eval('a', anchors =>
     anchors.map(a => ({
       href: a.href,
@@ -34,27 +49,67 @@ export async function webScraping({ url }: { url: string }) {
     }))
   )
 
-  // Ejemplo de cómo extraer Schema Markup JSON-LD
   const schemaMarkup = await page.$$eval('script[type="application/ld+json"]', scripts =>
     scripts.map(script => script.innerText)
   )
 
-  console.log('Title:', title)
-  console.log('Description:', description)
-  console.log('H1:', h1)
-  console.log('Images:', images)
-  console.log('Links:', links)
-  console.log('Schema Markup JSON-LD:', schemaMarkup)
-  console.log('-----------------------')
+  // console.log('Title:', title)
+  // console.log('Description:', description)
+  // console.log('H1:', h1)
+  // console.log('Images:', images)
+  // console.log('Links:', links)
+  // console.log('Schema Markup JSON-LD:', schemaMarkup)
+  // console.log('-----------------------')
 
   await browser.close()
 
+  let score = 100
+
+  const titleLength = title?.length
+  const descriptionLength = description?.length
+
+  if (!title) score -= 5
+
+  if (titleLength > 80 || titleLength < 55) {
+    score -= 3
+  }
+
+  if (titles.length > 0) score -= 3
+
+  if (!description) score -= 5
+
+  if (descriptionLength > 160 || descriptionLength < 119) {
+    score -= 3
+  }
+
+  if (!h1) score -= 5
+
+  images.forEach(img => {
+    if (img.alt.includes('No alt attribute')) score -= 3
+  })
+
+  const getDomainFromUrl = (url: string) => {
+    const urlObj = new URL(url)
+    return urlObj.hostname
+  }
+
+  const domainToCheck = getDomainFromUrl(url)
+  let countLinks = 0
+
+  links.forEach(link => {
+    if (link.href.includes(domainToCheck)) {
+      countLinks++
+    }
+  })
+
+  if (countLinks <= 0) score -= 5
+  if (countLinks <= 3) score -= 3
+
+  if (!schemaMarkup) score -= 5
+
+  console.log('Score:', score)
+
   return {
-    title,
-    description,
-    h1,
-    images,
-    links,
-    schemaMarkup,
+    score,
   }
 }
