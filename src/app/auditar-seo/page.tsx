fea577c481b134517cce7383a73b573333f8f10d
John Serrano <andrey_201015@hotmail.com>
2024-07-31 12:33:52 -0500

    Changes styles and new text

--- src/app/auditar-seo/page.tsx
+++ src/app/auditar-seo/page.tsx
@@ -1,5 +1,7 @@
+import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { InvalidWebAudit } from '@/components/InvalidWebAudit'
+import { WebAuditForm } from '@/components/WebAuditForm'
 import { webScraping } from '@/lib/scrapingPly'
 
 export default async function AuditSeo({
@@ -21,18 +23,20 @@ export default async function AuditSeo({
     solution: string
   }
 
-  const listIssues: Record<string, IssueDetail> = {
+  const issuesDescription: Record<string, IssueDetail> = {
     title: {
       value: 'Título',
-      description: 'El título de la página es muy largo o muy corto.',
-      solution: 'Ajusta el título de la página para que sea lo más descriptivo posible.',
+      description:
+        'Falta la etiqueta `<title>` en el HTML de la página. Esta etiqueta es crucial para el SEO ya que define el título que aparece en la pestaña del navegador y en los resultados de búsqueda.',
+      solution:
+        'Agrega una etiqueta `<title>` en el `<head>` del HTML con un título descriptivo y relevante para la página.',
     },
     titleLength: {
       value: 'Título largo',
       description: 'El título de la página es muy largo o muy corto.',
       solution: `Ajusta el título de la página. Tiene ${
         issues.find(item => item.key.includes('titleLength'))?.count
-      } caracteres.`,
+      } caracteres. Se recomienda que tenga entre 55 a 80 caracteres.`,
     },
     titles: {
       value: 'Títulos',
@@ -43,20 +47,29 @@ export default async function AuditSeo({
     },
     description: {
       value: 'Descripción',
+      description:
+        'La etiqueta `<meta name="description">` está ausente en el HTML de la página. Esta metaetiqueta proporciona una breve descripción del contenido de la página, la cual puede ser utilizada por los motores de búsqueda en los resultados de búsqueda y en las redes sociales.',
+      solution:
+        'Agrega una etiqueta `<meta name="description" content="Descripción breve y relevante del contenido de la página.">` en el `<head>` del HTML.',
+    },
+    descriptionLength: {
+      value: 'Descripción larga',
       description: 'La descripción de la página es muy larga o muy corta.',
-      solution: 'Ajusta la descripción de la página para que sea lo más descriptivo posible.',
+      solution: `Ajusta la descripción de la página. Tiene ${
+        issues.find(item => item.key.includes('descriptionLength'))?.count
+      } caracteres. Se recomienda que tenga entre 120 y 160 caracteres.`,
     },
     h1: {
       value: 'H1',
       description: 'El encabezado H1 no es adecuado para la página.',
-      solution: 'Ajusta el encabezado H1 para que sea lo más descriptivo posible.',
+      solution: 'Agrega una etiqueta `<h1>` en el cuerpo de la página.',
     },
     h2: {
       value: 'H2',
       description: 'El encabezado H2 no es adecuado para la página.',
-      solution: 'Ajusta el encabezado H2 para que sea lo más descriptivo posible.',
+      solution: 'Agrega una etiqueta `<h2>` en el cuerpo de la página.',
     },
-    alt: {
+    images: {
       value: 'Alt en la imágenes',
       description: 'Las imágenes no tienen atributos alt.',
       solution: 'Agrega atributos alt a las imágenes para mejorar la accesibilidad.',
@@ -73,6 +86,17 @@ export default async function AuditSeo({
     },
   }
 
+  type ImageDetail = { src: string; alt: string }
+  type LinkDetail = { href: string; text: string }
+
+  function isImageDetail(item: any): item is ImageDetail {
+    return item && typeof item.src === 'string' && typeof item.alt === 'string'
+  }
+
+  function isLinkDetail(item: any): item is LinkDetail {
+    return item && typeof item.href === 'string' && typeof item.text === 'string'
+  }
+
   return (
     <>
       <Header />
@@ -81,7 +105,8 @@ export default async function AuditSeo({
           Audita el <span className='dark:text-rose-600 text-rose-700'>SEO</span> de tu sitio web 🔥
         </h1>
 
-        <div className='flex items-center flex-col'>
+        <header className='flex items-center flex-col'>
+          <h2 className='text-center text-2xl font-light mb-3'>{url}</h2>
           <div className='relative size-40'>
             <svg className='rotate-[135deg] size-full' viewBox='0 0 36 36' xmlns='http://www.w3.org/2000/svg'>
               <circle
@@ -100,7 +125,13 @@ export default async function AuditSeo({
                 cy='18'
                 r='16'
                 fill='none'
-                className='stroke-current text-green-500 dark:text-green-500'
+                className={`stroke-current ${
+                  score > 84
+                    ? 'text-green-600 dark:text-green-500'
+                    : score > 45
+                    ? 'text-orange-600 dark:text-orange-500'
+                    : 'text-red-600 dark:text-red-500'
+                }`}
                 strokeWidth='2'
                 strokeDasharray={score}
                 strokeLinecap='round'
@@ -108,25 +139,72 @@ export default async function AuditSeo({
             </svg>
 
             <div className='absolute top-1/2 start-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
-              <span className='text-4xl font-bold text-green-600 dark:text-green-500'>{score}</span>
-              <span className='text-green-600 dark:text-green-500 block'>Puntaje</span>
+              <span
+                className={`text-4xl font-bold ${
+                  score > 85
+                    ? 'text-green-600 dark:text-green-500'
+                    : score > 45
+                    ? 'text-orange-600 dark:text-orange-500'
+                    : 'text-red-600 dark:text-red-500'
+                }`}
+              >
+                {score}
+              </span>
+              <span
+                className={`${
+                  score > 85
+                    ? 'text-green-600 dark:text-green-500'
+                    : score > 45
+                    ? 'text-orange-600 dark:text-orange-500'
+                    : 'text-red-600 dark:text-red-500'
+                } block`}
+              >
+                Puntaje
+              </span>
             </div>
           </div>
-          <p className='mt-2'>Puntuación de SEO en la página</p>
-        </div>
+          <div className='flex gap-2 items-center mt-3'>
+            <p>Puntuación de SEO en la página</p>
+            <div className='group relative'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                className=' text-gray-500 w-3 h-3 cursor-pointer'
+              >
+                <circle cx='12' cy='12' r='10'></circle>
+                <path d='M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3'></path>
+                <path d='M12 17h.01'></path>
+              </svg>
+              <div
+                role='tooltip'
+                className='tooltip absolute z-10 invisible w-60 inline-block px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 pointer-events-none select-none -top-28 left-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300'
+              >
+                Solo se consideran problemas individuales para el puntaje. Por ejemplo, cada imagen sin atributo alt se
+                cuenta como un problema.
+              </div>
+            </div>
+          </div>
+        </header>
 
         <div className='bg-blue-100 dark:bg-transparent dark:border dark:border-blue-500 w-full min-h-max rounded px-7 py-5 mt-12'>
           <span className='text-slate-800 dark:text-white text-2xl'>Problemas</span>
-          <ul className='mt-5 flex flex-col gap-5'>
+          <ul className='mt-5 flex flex-col gap-7'>
             {issues.map(({ issue, key, detail }) => {
               if (!issue) return
 
               console.log({ issue, key, detail })
-              // console.log(listIssues[key])
+              // console.log(issuesDescription[key])
               return (
                 <li key={key} className=''>
                   <h3 className='font-medium leading-6 text-slate-800 dark:text-white capitalize'>
-                    {listIssues[key].value}
+                    {issuesDescription[key].value}
                   </h3>
                   {key === 'titles' && Array.isArray(detail) && (
                     <div className='mt-1 flex flex-col rounded-md shadow-sm px-2 py-2 break-all max-h-28 overflow-scroll whitespace-pre-line border border-red-600 bg-transparent'>
@@ -139,10 +217,13 @@ export default async function AuditSeo({
                   {key === 'images' && Array.isArray(detail) && (
                     <div className='mt-1 flex rounded-md shadow-sm px-2 py-2 break-all max-h-28 overflow-scroll whitespace-pre-line border border-red-600 bg-transparent'>
                       {detail.map((item, index) => (
-                        <div key={index}>
-                          {item?.src ? item.src : null}
-                          {item?.alt ? item.alt : null}
-                        </div>
+                        <ul key={index}>
+                          {isImageDetail(item) && (
+                            <li className='flex gap-3'>
+                              <span>{item.src}</span>
+                            </li>
+                          )}
+                        </ul>
                       ))}
                     </div>
                   )}
@@ -150,10 +231,13 @@ export default async function AuditSeo({
                   {key === 'links' && Array.isArray(detail) && (
                     <div className='mt-1 flex rounded-md shadow-sm px-2 py-2 break-all max-h-28 overflow-scroll whitespace-pre-line border border-red-600 bg-transparent'>
                       {detail.map((item, index) => (
-                        <div key={index}>
-                          {item.href ? item.href : null}
-                          {item.text ? item.text : null}
-                        </div>
+                        <ul key={index}>
+                          {isLinkDetail(item) && (
+                            <li className='flex gap-3'>
+                              <span>{item.href}</span>
+                            </li>
+                          )}
+                        </ul>
                       ))}
                     </div>
                   )}
@@ -164,13 +248,20 @@ export default async function AuditSeo({
                     </div>
                   )}
 
-                  <div className='text-red-600 mt-2'>{listIssues[key].solution}</div>
+                  <div className='text-red-600 mt-2'>{issuesDescription[key].solution}</div>
                 </li>
               )
             })}
           </ul>
         </div>
+
+        <footer className='w-full mt-20'>
+          <h3 className='text-4xl my-4 text-center'>Auditar otro sitio web</h3>
+          <WebAuditForm />
+        </footer>
       </main>
+
+      <Footer />
     </>
   )
 }
